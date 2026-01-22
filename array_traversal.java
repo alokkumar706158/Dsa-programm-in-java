1265bab090b58b5637b73c0037cd5ec62a1d70ee
Alok Kumar <alokkumar706158@gmail.com>
2026-01-22 09:14:32 +0530

    reverse string commit

---
+++ array_traversal.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class array_traversal{
+    public static void main(String[] args) {
+        System.out.println("Enter size of array");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println("Enter an array");
+        int arr[]=new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i]+" ");
+        }  
+        
+    }
+}
---
+++ reverse_vowels.java
@@ -0,0 +1,28 @@
+public class reverse_vowels {
+    public String reverseVowels(String s) {
+        String vowels = "aeiouAEIOU";
+        char[] arr = s.toCharArray();
+        
+        int l = 0, r = arr.length - 1;
+        
+        while (l < r) {
+            if (!vowels.contains(arr[l] + "")) {
+                l++;
+                continue;
+            }
+            if (!vowels.contains(arr[r] + "")) {
+                r--;
+                continue;
+            }
+            
+            char temp = arr[l];
+            arr[l] = arr[r];
+            arr[r] = temp;
+            
+            l++;
+            r--;
+        }
+        
+        return new String(arr);
+    }
+}
